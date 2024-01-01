e51830881f08038dbf43ac4b968bd52d8168a6c7
funkycadet <ericsmithsonian2@gmail.com>
2024-01-01 23:56:35 +0100

    task: add middleware

---
+++ src/middleware/validateReqBody.ts
@@ -0,0 +1,27 @@
+import joi, { Schema, ValidationOptions } from "joi";
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+export default (schema: Schema): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const validationOptions: ValidationOptions = {
+        abortEarly: false,
+        allowUnknown: true,
+        stripUnknown: true,
+      };
+
+      const validated = await schema.validateAsync(req.body, validationOptions);
+
+      req.body = validated;
+
+      next();
+    } catch (err: any) {
+      console.log(err);
+      const errMessages: string[] = [];
+      err.details.map((e: any) => errMessages.push(e.message));
+      err.statusCode = 409;
+      err.message = errMessages.join(",");
+      next(err);
+    }
+  };
+};
