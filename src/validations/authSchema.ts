cba37e6f8452a86fdc303e83a99253e74c5d659c
Eric Alaribe <ericsmithsonian2@gmail.com>
2024-08-06 13:15:18 +0100

    patch: add gender field to auth schema

--- src/validations/authSchema.ts
+++ src/validations/authSchema.ts
@@ -1,11 +1,14 @@
 import Joi from "joi";
 
+const gender = ["male", "female"]
+
 export const signupSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   email_address: Joi.string().email().required(),
   phone_number: Joi.string().required(),
   password: Joi.string().required().min(8),
+  gender: Joi.string().valid(...gender).required(),
   // role: Joi.string().valid("user", "admin").required(),
 });
 
