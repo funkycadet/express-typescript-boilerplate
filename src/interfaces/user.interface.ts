d43d55e7caff2d1f2ac395265d295790b2cf77e6
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-02-15 11:28:24 +0100

    task: update user interfaces

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,13 +1,35 @@
-import { Role } from "@prisma/client";
+import { Prisma, Role } from '@prisma/client';
 
-export interface IUser {
-  id?: string;
+// export interface IUser {
+//   id?: string;
+//   firstName: string;
+//   lastName: string;
+//   email_address: string;
+//   phone_number: string;
+//   password: string;
+//   gender: string;
+//   roles?: Role[];
+//   refreshTokens?: string[];
+// }
+//
+export type IUser = Omit<Prisma.$UserPayload['scalars'], ''>;
+
+export interface IUserSignup {
   firstName: string;
   lastName: string;
-  email_address: string;
-  phone_number: string;
+  emailAddress: string;
   password: string;
   gender: string;
-  roles?: Role[];
-  refreshTokens?: string[];
+  role?: Role;
+}
+
+export interface IUserLogin {
+  emailAddress: string;
+  password: string;
+}
+
+export interface IUpdateUserPassword {
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
 }
