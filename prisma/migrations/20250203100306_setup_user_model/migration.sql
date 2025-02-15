f0a36ec97b9825136ffbe04812a3e31f79f07c42
Eric Alaribe <ericsmithsonian2@gmail.com>
2025-02-15 11:09:43 +0100

    patch: update user model and seed file

--- prisma/migrations/20250203100306_setup_user_model/migration.sql
+++ prisma/migrations/20250203100306_setup_user_model/migration.sql
@@ -1,16 +1,15 @@
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('user', 'admin');
+CREATE TYPE "Role" AS ENUM ('user', 'admin', 'superadmin');
 
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
-    "email_address" TEXT NOT NULL,
-    "phone_number" TEXT NOT NULL,
+    "emailAddress" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" "Role" DEFAULT 'user',
-    "refreshTokens" TEXT[],
+    "gender" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'user',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -18,4 +17,4 @@ CREATE TABLE "User" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_address_key" ON "User"("email_address");
+CREATE UNIQUE INDEX "User_emailAddress_key" ON "User"("emailAddress");
